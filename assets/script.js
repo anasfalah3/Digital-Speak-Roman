fef707ae3f4e4a4db62ef0189dfe945a78345c52
anasfalah3 <anasfalah3@gmail.com>
2024-06-16 16:55:24 +0100

    add animations

---
+++ assets/script.js
@@ -0,0 +1,27 @@
+$(document).ready(function () {
+  var fadeSections = $(".services,.about, .portfolio, .features, .footer");
+
+  fadeSections.css("opacity", 0);
+
+  $(window).scroll(function () {
+    var scrollPosition = $(window).scrollTop();
+
+    fadeSections.each(function (index, element) {
+      var section = $(element);
+      var sectionTop = section.offset().top - $(window).height() / 3;
+
+      if (scrollPosition > sectionTop) {
+        section.stop().animate(
+          {
+            opacity: 1,
+          },
+          200
+        );
+      }
+    });
+  });
+});
+
+setTimeout(function () {
+  $(".loader").fadeOut(500);
+}, 1000);
